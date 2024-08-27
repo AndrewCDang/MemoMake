71c91b4c686c8181a27895320b2032bebabcc3f4
Andrew Dang <andrew-dang2@hotmail.co.uk>
2024-08-27 19:30:09 +0100

    next config path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+// next.config.js
+const path = require("path");
+
+module.exports = {
+    webpack(config) {
+        config.resolve.alias["@"] = path.resolve(__dirname, "src");
+        return config;
+    },
+};
